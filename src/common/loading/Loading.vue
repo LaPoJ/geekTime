73a176c446f4115bd28d6f3046032138d3d96e81
LP_ZX <2295140520@qq.com>
2019-08-12 23:28:13 +0800

    feat(component): add lazy load component(common)
    页面请求的数据未完成时进行加载显示

---
+++ src/common/loading/Loading.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="loading" v-show="$store.state.isLoading">
+    <van-loading color="#1989fa" class="van-laoding" type="spinner" />
+  </div>
+</template>
+
+<script>
+import { Loading } from 'vant'
+export default {
+  data () {
+    return {
+
+    }
+  },
+  components: {
+    'van-loading': Loading
+  }
+}
+</script>
+
+<style scope>
+.loading{
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  background-color:#E6E6E6;
+  opacity: 0.7;
+}
+.van-loading{
+  z-index: 1;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+}
+
+</style>
