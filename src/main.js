6a99038646f3cf5b031b1f3d622ce03a52802fc9
LP_ZX <2295140520@qq.com>
2019-08-12 23:34:27 +0800

    feat(app): add main of app

--- src/main.js
+++ src/main.js
@@ -2,17 +2,27 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import Vant from 'vant'
+
+import Vuex from 'vuex'
+import axios from 'axios'
+
 import router from './router'
+import store from './store/index'
+
+// eslint-disable-next-line no-unused-vars
+import Vant from 'vant'
 import 'vant/lib/index.css'
 
 Vue.config.productionTip = false
-Vue.use(Vant)
+Vue.prototype.$http = axios
+
+Vue.use(Vuex)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
