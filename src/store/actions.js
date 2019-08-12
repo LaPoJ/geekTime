fb0218e670da0afb7c7bf954d257079796f78888
LP_ZX <2295140520@qq.com>
2019-08-12 23:39:27 +0800

    feat(model): add action module for vuex
    分离 action 模块

---
+++ src/store/actions.js
@@ -0,0 +1,7 @@
+const actions = {
+  // user_signin: ({ commit }, user) => commit('user_signin')
+  changeShowTabbar: ({ commit }) => commit('changeShowTabbar'),
+  changeLoading: ({ commit }) => commit('changeLoading')
+}
+
+export default actions
