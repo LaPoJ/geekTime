4ec94bc31047c096882777430abd2e82cdadc3db
LP_ZX <2295140520@qq.com>
2019-08-12 23:37:50 +0800

    feat(model): add vuex
    vuex 模块的入口文件

---
+++ src/store/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import actions from './actions'
+import mutations from './mutations'
+import getters from './getters'
+
+Vue.use(Vuex)
+
+const state = {
+  isShowTabbar: true,
+  isLoading: true
+}
+
+const store = new Vuex.Store({
+  state,
+  actions,
+  mutations,
+  getters
+})
+
+export default store
