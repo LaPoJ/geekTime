b33ecbbd5cbabba8955c396c408b2a1e6885757e
LP_ZX <2295140520@qq.com>
2019-08-13 22:13:54 +0800

    feat(componet): add bottom tabbar
    添加底部 tabbar 组件

---
+++ src/components/navbar/TabbarBootom.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="navbar" v-show="$store.state.isShowTabbar">
+    <van-tabbar v-model="active">
+      <van-tabbar-item replace to="/index" icon="wap-home">讲堂</van-tabbar-item>
+      <van-tabbar-item replace to="/learn" icon="label-o">学习</van-tabbar-item>
+      <van-tabbar-item replace to="/mine" icon="contact">我的</van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+
+<script>
+import { Tabbar, TabbarItem } from 'vant'
+
+export default {
+  name: 'Tabbar',
+  data () {
+    return {
+      active: 0
+    }
+  },
+  components: {
+    'van-tabbar': Tabbar,
+    'van-tabbar-item': TabbarItem
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
