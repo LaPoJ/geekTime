7bac3bb424d78658de9e6e4a03ed7315984b3741
LP_ZX <2295140520@qq.com>
2019-08-13 22:11:02 +0800

    feat(components): add column card view
    add the info of column

---
+++ src/components/intro/ColumnIntro.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="intro-page">
+    <van-nav-bar
+      title="标题"
+      left-text="返回"
+      right-text="按钮"
+      left-arrow
+      @click-left="onClickBack"
+      @click-right="onShare"
+    />
+  </div>
+</template>
+
+<script>
+import { NavBar } from 'vant'
+export default {
+  components: {
+    'van-nav-bar': NavBar
+  },
+  methods: {
+    onClickBack () {
+      this.$router.go(-1)
+    },
+    onShare () {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
